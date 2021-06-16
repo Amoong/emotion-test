0825f132a2a724d488843db518ac8e9a4ea1500d
Amoong <moo1323@naver.com>
2021-06-16 12:36:04 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+import styles from "./App.module.scss";
+
+import Nav from "./Nav";
+import AboutPage from "./pages/AboutPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isDarkTheme ? "dark" : "light"}>
+      <div className={styles.App}>
+        <h1 className={styles.title}>css-module</h1>
+        <button size="small" onClick={() => setIsDarkTheme(!isDarkTheme)}>
+          {isDarkTheme ? "🌙" : "☀️"}
+        </button>
+        <Router>
+          <Nav />
+          <Switch>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route path="/">
+              <MainPage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import styles from "./MainPage.module.scss";
+
+function MainPage() {
+  return (
+    <div className={styles.MainPage}>
+      <h1 className={styles.title}>Main Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+        earum!
+      </p>
+    </div>
+  );
+}
+
+export default MainPage;
